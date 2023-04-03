ecdf247afecc6f39f5926a5784e38d599cbc5b12
Tipanoi-Janet <tipanoijanet@gmail.com>
2023-04-03 10:37:10 +0300

    first commit

---
+++ merge.js
@@ -0,0 +1,36 @@
+function divideArray(arr){
+    if(arr.length)<=1)
+    return arr;
+
+}   
+
+let middle =Math floor(arc.length/2)
+let left = arr.slice(o, middle);
+let right = arr.slice(middle);
+return sortedArray(divideArray(left),divideArray(right));
+
+]
+
+function sortedArray(left,right)[
+    let emptyArr ={};
+    while(left.length&& right.length)
+    if(left[0]){
+        emptyArr.push(left.shift())
+        else{
+            emptyArr.push(right.shift())
+
+
+
+        }
+        console.log((emptyArr));
+        return {...emptyArr});
+
+
+    }
+    
+
+
+
+        
+    
+
